2285b0860539e1c2f231a6cb5370b04f9a428667
hellovokzal <100135590+hellovokzal@users.noreply.github.com>
2023-05-21 09:38:47 +0500

    Create dstat.py

---
+++ dstat.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+import os
+os.system("pip install Flask")
+os.system("pip2 install Flask")
+os.system("pip3 install Flask")
+app = Flask(__name__)
+num = 0
+
+@app.route('/')
+def index():
+    global num
+    num = num + 1
+    return str(num)
+
+if __name__ == '__main__':
+    app.run()
